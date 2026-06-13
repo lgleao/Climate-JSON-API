d6159215ef6267560aabf8b1266ad2c4e15c48d5
Gustavo Leão <gustavodleao.dev@gmail.com>
2026-06-13 15:10:28 -0300

    Refactor city search and improve JSON handling

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ def clear():
     else:
         os.system("clear")
 
+def busca(city, dados):
+    index = -1
+    for i in range(len(dados)):
+        if(dados[i]['cidade'] == city):
+            return i
+    return -1
+
 clear()
 
 try:
@@ -24,13 +31,13 @@ except:
 #Action options
 while True:
     options()
-    escolha = int(input("Type the wanted option: "))
+    escolha = input("Type the wanted option: ")
     #"Run the program"
-    if escolha == 1:
+    if escolha == "1":
         clear()
         break
     #"Register new location"
-    elif escolha == 2:
+    elif escolha == "2":
         cidade = input("Type the city name: ").upper()
         latitude = input("Type the latitude: ")
         longitude = input("Type the longitude: ")
@@ -47,45 +54,52 @@ while True:
             dados = []
 
         dados.append(nova_cidade)
-        with open("dados.json", "w") as arquivo:
-            json.dump(dados,arquivo,indent=4)
+        with open("dados.json", "w", encoding= "utf-8") as arquivo:
+            json.dump(dados,arquivo,indent=4, ensure_ascii=False, sort_keys=False)
         clear()
     else:
-        print("Inválido.")
+        print("Invalid.")
         continue
 
 #Search coordinates on the JSON file
 cidade_encontrada = None
+pull_city = input("Which city? ").upper()
+index = busca(pull_city, dados)
+if (index > -1):
+    cid = dados[index]
+    print(cid["cidade"])
+    print(cid["longitude"])
+    print(cid["latitude"])
+
 for cidade in dados:
     if cidade["cidade"] == pull_city:
         cidade_encontrada = cidade
         latitude = cidade_encontrada["latitude"]
         longitude = cidade_encontrada["longitude"]
         break
-    else:
-        cidade_encontrada = cidade
-        latitude = cidade_encontrada["latitude"]
-        longitude = cidade_encontrada["longitude"]
+
+if cidade_encontrada is None:
+    print("City not found. Please register it.")
+    exit()
     
-    #API connection
-    url = (f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true")
+#API connection
+url = (f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true")
 
-    response = requests.get(url)
-    dados_api = response.json()
+clear()
 
-    temperatura = dados_api["current_weather"]["temperature"]
-    clima = dados_api["current_weather"]["is_day"]
-    if clima == 1:
-        clima = "day"
-    else:
-        clima = "night"
-    #Information output for the user
-    print(
-        f"The current temperature in {cidade_encontrada["cidade"]} is {temperatura}°C\n"
-        f"At the moment, {cidade_encontrada["cidade"]} is {clima}.\n"
-        )
+print("Working...")
 
+response = requests.get(url)
+dados_api = response.json()
 
-
-
-
+temperatura = dados_api["current_weather"]["temperature"]
+clima = dados_api["current_weather"]["is_day"]
+if clima == 1:
+    clima = "day"
+else:
+    clima = "night"
+#Information output for the user
+print(
+    f"The current temperature in {cidade_encontrada["cidade"]} is {temperatura}°C\n"
+    f"At the moment, {cidade_encontrada["cidade"]} is {clima}.\n"
+    )
